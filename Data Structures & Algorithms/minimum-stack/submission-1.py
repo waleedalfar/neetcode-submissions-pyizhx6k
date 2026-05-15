11c1dd584096693595c153c5f748410dadbe3654
Waleed <161538340+waleedalfar@users.noreply.github.com>
2026-05-15 15:56:13 -0700

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.py
@@ -0,0 +1,21 @@
+class MinStack:
+
+    def __init__(self):
+        self.ints = []
+        self.min = []
+
+    def push(self, val: int) -> None:
+        self.ints.append(val)
+        if self.min: 
+            val = min(val, self.min[-1])
+        self.min.append(val)
+
+    def pop(self) -> None:
+        self.ints.pop()
+        self.min.pop()
+
+    def top(self) -> int:
+        return self.ints[-1]
+
+    def getMin(self) -> int:
+        return self.min[-1]
